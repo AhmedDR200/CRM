16adc0a496fbe0706dfe7d117ae216266bca86e9
Ahmed Magdy <94301115+AhmedDR200@users.noreply.github.com>
2023-07-27 23:41:19 +0300

    deployment setup2

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 
 echo "BUILD START"
-python3.11.4 -m pip -r requirements.txt
-python3.11.4 manage.py collectstatic --noinput --clear
+python3.9 -m pip -r requirements.txt
+python3.9 manage.py collectstatic --noinput --clear
 echo "BUILD END"
