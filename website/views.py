80e316e047681ffc2cd7a9326f9f3e42d08a0e7e
Ahmed Magdy <94301115+AhmedDR200@users.noreply.github.com>
2023-07-27 20:15:54 +0300

    Templates setup2

--- website/views.py
+++ website/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-
+from django.contrib.auth import authenticate , login , logout
+from django.contrib import messages
 # Create your views here.
 
 
 def home(request):
     return render(request, 'home.html', {} )
+
+
+
+def logout_user(request):
+    pass
